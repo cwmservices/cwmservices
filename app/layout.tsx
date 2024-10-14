c0efd7dbe87954039b956762c34c0f25faefae58
cwmservices <masoodkhan224488@gmail.com>
2024-10-14 16:32:39 +0500

    fixed analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter, Poppins } from "next/font/google";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({
   subsets: ["latin"],
@@ -29,6 +30,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.variable} ${poppins.variable}`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
