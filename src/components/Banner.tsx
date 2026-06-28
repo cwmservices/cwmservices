ab9efeaa4284cc23b7472c91a423fdbf4cdf5ae9
masood-u <masoodkhan224488@gmail.com>
2026-06-28 23:01:09 +0500

    Fix spline-viewer type definition

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -8,7 +8,11 @@ declare global {
   namespace JSX {
     interface IntrinsicElements {
       "spline-viewer": React.DetailedHTMLProps<
-        React.HTMLAttributes<HTMLElement> & { url?: string },
+        React.HTMLAttributes<HTMLElement> & {
+          url?: string;
+          loading?: string;
+          "loading-anim-type"?: string;
+        },
         HTMLElement
       >;
     }
